4a14ee3d9706545586af8b2ca8490240a972a4b3
Aswin Biju <aswinbijux@gmail.com>
2024-12-28 15:19:58 +0530

    Handling invalid inputs

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,53 +1,7 @@
 using GameStore.Api.Dtos;
+using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-const string GetGameEnpointName = "GetGame";
-
-List<GameDto> games=[
-    new(1,"God of war","Fighting",20.00M,new DateOnly(1992,7,15)),
-    new(2,"Fifa 24","Sports",26.00M,new DateOnly(1992,7,15)),
-    new(3,"God of war","Fighting",20.00M,new DateOnly(1992,7,15)),
-    new(4,"God of war","Fighting",20.00M,new DateOnly(1992,7,15))
-];
-app.MapGet("/games",() => games);
-app.MapGet("/games/{id}",(int id)=>{
-
-    GameDto? game = games.Find(game=>game.Id==id);
-    return game is null? Results.NotFound() : Results.Ok(game);
-    })
-    .WithName(GetGameEnpointName);
-
-
-app.MapPost("/games",(CreateGameDto newGame)=>{
-    GameDto game = new(
-        games.Count + 1,
-        newGame.Name, 
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-    );
-    games.Add(game);
-    return Results.CreatedAtRoute(GetGameEnpointName,new {id = game.Id},game);
-});
-app.MapPut("/games/{id}",(int id,UpdateGameDto updatedGame)=>{
-    var index = games.FindIndex(game=>game.Id == id);
-    if(index == -1)
-    {
-        return Results.NotFound(); // you could also create a new resource instead of just returning not found.
-    }
-    games[index] = new GameDto(
-        id,
-        updatedGame.Name,
-        updatedGame.Genre,
-        updatedGame.Price,
-        updatedGame.ReleaseDate
-    );
-    return Results.Json(games[index]);
-});
-app.MapDelete("/games/{id}",(int id)=>{
-    games.RemoveAll(game=>game.Id == id);
-    return Results.Ok();
-});
-
+app.MapGamesEndpoints();
 app.Run();
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c6d7958d87edac6cf741ce844e1372b249c7ac0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b379b323a37abbe88fb632935a91d68d26c1b3b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
